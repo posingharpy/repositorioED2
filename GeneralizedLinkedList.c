1153c2d3df2a4123c4e36ab9a9bc2eb358c0a773
posingharpy <158221652+posingharpy@users.noreply.github.com>
2024-03-15 19:27:24 +0000

    finalizado lista generalizada

--- GeneralizedLinkedList.c
+++ GeneralizedLinkedList.c
@@ -9,6 +9,16 @@ int addAtom(Node **list, int atom)
     newNode->type = 0;
     newNode->atomList.atom = atom;
     newNode->next = NULL;
+    if (*list==NULL)
+    {
+        *list=newNode;
+    }
+    else
+    {
+        Node *aux=NULL;
+        for (aux= *list; aux->next!=NULL; aux=aux->next);
+        aux->next=newNode;
+    }
     return 1;
 }
 
@@ -19,8 +29,74 @@ int addList(Node **list, Node **subList)
     newNode->type = 1;
     newNode->atomList.list = *subList;
     newNode->next = NULL;
+    if (*list==NULL)
+    {
+        *list=newNode;
+    }
+    else
+    {
+        Node *aux=NULL;
+        for (aux= *list; aux->next!=NULL; aux=aux->next);
+        aux->next=newNode;
+    }
     return 1;
 }
 
+Node* head(Node *list)
+{
+    Node *headaux=NULL;
+    if ((list)->type==0)
+    {
+        headaux=(Node*)malloc(sizeof(Node));
+        headaux->type=0;
+        headaux->atomList.atom=(list)->atomList.atom;
+        headaux->next=NULL;
+    }
+    else
+    {
+        headaux=list->atomList.list;
+    }
+    return headaux;
+}
 
+Node* tail(Node *list)
+{
+    return (list)->next;
+}
 
+void show(Node *list)
+{
+    printf("( ");
+    Node *aux = NULL;
+    for (aux = list; aux!=NULL; aux=aux->next)
+    {
+        if (aux->type==0)
+        {
+            printf("%d ",aux->atomList.atom);
+        }
+        else
+        {
+            show(aux->atomList.list);
+        }
+    }
+    printf(")");
+}
+
+int depth(Node *list)
+{
+    int deep=0;
+    if (list==NULL)
+        return 0;
+
+    Node *aux=NULL;
+    for (aux=list; aux!=NULL; aux=aux->next)
+    {
+        if (aux->type==1)
+        {
+            int prof = depth(aux->atomList.list);
+            if (prof>deep)
+                deep=prof;
+        }
+    }
+    return deep+1;
+}
--- GeneralizedLinkedListTest.c
+++ GeneralizedLinkedListTest.c
@@ -3,10 +3,31 @@
 #include "GeneralizedLinkedList.h"
 
 int main()
-{
-    addAtom();
+{   
+    Node *listaGeneralizada = NULL;
+    printf("Lista: ");
+    show(listaGeneralizada);
+    addAtom(&listaGeneralizada, 3);
+    addAtom(&listaGeneralizada, 5);
+    Node *lista2 = NULL;
+    addAtom(&lista2, 7);
+    addAtom(&lista2, 9);
+    Node *lista3 = NULL;
+    addAtom(&lista3, 11);
+    addAtom(&lista3, 13);
 
+    addList(&listaGeneralizada, &lista2);
+    addList(&listaGeneralizada, &lista3);
+
+    printf("\nLista: ");
+    show(listaGeneralizada);
+    printf("\nProfundidade: %d\n",depth(listaGeneralizada));
     
+    printf("\nTail: ");
+    show(tail(listaGeneralizada));
 
-    return EXIT_SUCCESS;
+    printf("\n\nhead(tail()): %d\n",head(tail(listaGeneralizada))->atomList.atom);
+    printf("\n\nhead(tail(tail(head(tail(tail()))))):\n");
+    
+    return 0;
 }
