90a2771bf452ba9a5000f9e884536ef75512eb18
posingharpy <158221652+posingharpy@users.noreply.github.com>
2024-03-15 20:00:14 +0000

    adicionado hash

---
+++ hash.h
@@ -0,0 +1,21 @@
+#ifndef Estruturadedados_Hash_h
+#define Estruturadedados_Hash_h
+#include "DoublyLinkedList.h"
+#define MAX 10
+
+typedef struct HashStruct
+{
+    DoublyLinkedList hashes[MAX];
+    int size;
+}HashStruct;
+
+void initHash(HashStruct *hashStruct);
+bool isHashEmpty(HashStruct *hashStruct);
+int hash(char *key);
+int put(HashStruct *hashStruct, char *key, void *data, compare equal);
+bool containsKey(HashStruct *hashStruct, char *key, compare equal);
+void* get(HashStruct *hashStruct, char *key, compare equal);
+void* removeKey(HashStruct *hashStruct, char *key, compare equal);
+void show(HashStruct *hashStruct);
+
+#endif
